e837c81e78edcd7b4d64e58ee21096e46492617e
Jide-codes <hezekiaholajide3@gmail.com>
2024-03-22 05:41:34 -1100

    preparing for deployment

--- StudyLounge/settings.py
+++ StudyLounge/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
 STATICFILES_DIRS = BASE_DIR,'static'
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
