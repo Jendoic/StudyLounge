ff44ead1415e589f6f15285589a0796b0c161522
Debug Titan <127851912+debugtitan@users.noreply.github.com>
2024-03-23 12:35:15 +0100

    Allowed host phase1: .vercel.app

--- StudyLounge/settings.py
+++ StudyLounge/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
