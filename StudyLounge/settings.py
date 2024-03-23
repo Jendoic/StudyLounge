84fa4d24804c3722a497e6f216fb9a48dd3b9c6b
Hezekiah Olajide <hezekiaholajide3@gmail.com>
2024-03-23 03:47:22 -1100

    Update settings.py

--- StudyLounge/settings.py
+++ StudyLounge/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_soft.apps.AdminSoftDashboardConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
